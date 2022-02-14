00d4d7be1698113557dea690cc87916bf6dc65fe
Alexandr Mun <munakaz@yandex.ru>
2022-02-14 23:03:33 +0600

    CH4

--- src/main/java/kz/bitlab/m3_ch1/repository/DBManager.java
+++ src/main/java/kz/bitlab/m3_ch1/repository/DBManager.java
@@ -12,11 +12,12 @@ public class DBManager implements StudentRepository {
     private static List<Student> studentList = new ArrayList<>();
 
     static {
-        studentList.add(new Student(1L, "Begarys", "2000-04-18", "Almaty"));
-        studentList.add(new Student(2L, "Aisulu", "2000-04-18", "Almaty"));
-        studentList.add(new Student(3L, "Beka", "2000-04-18", "Almaty"));
-        studentList.add(new Student(4L, "Abzal", "2000-04-18", "Almaty"));
-        studentList.add(new Student(5L, "Islam", "2000-04-18", "Almaty"));
+        studentList.add(new Student(1L, "Azhar", "2000-04-18", "Shimkent"));
+        studentList.add(new Student(2L, "Begarys", "2000-04-18", "Almaty"));
+        studentList.add(new Student(3L, "Aisulu", "2000-04-18", "Almaty"));
+        studentList.add(new Student(4L, "Beka", "2000-04-18", "Almaty"));
+        studentList.add(new Student(5L, "Abzal", "2000-04-18", "Almaty"));
+        studentList.add(new Student(6L, "Islam", "2000-04-18", "Almaty"));
     }
 
     private static Long id = 6L;
